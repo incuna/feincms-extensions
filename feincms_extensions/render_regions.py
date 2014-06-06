70e53187c2cc45126565bf46f15b89146da184b3
Ian Foote <ian@incuna.com>
2014-06-06 10:31:31 +0100

    Remove unhelpful comments

--- feincms_extensions/render_regions.py
+++ feincms_extensions/render_regions.py
@@ -7,12 +7,9 @@ class Extension(extensions.Extension):
 
         def rendered_regions(self, request):
             """Render the feincms regions into a dictionary."""
-            # Loop over the template regions.
             regions = {}
             for region in self.template.regions:
-                # Get the content instances associated with the region.
                 content_list = getattr(self.content, region.key)
-                # Render all the content, and join into one string.
                 content = [c.render(request=request) for c in content_list]
                 regions[region.key] = ' '.join(content)
             return regions
