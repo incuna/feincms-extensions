1905c3bc05119e124d885f8944af134cfe77f661
Ian Foote <ian@incuna.com>
2015-03-06 12:03:25 +0000

    Add check for content types' `.json` method

--- feincms_extensions/render_json.py
+++ feincms_extensions/render_json.py
@@ -1,3 +1,4 @@
+from django.core import checks
 from feincms import extensions
 
 
@@ -16,3 +17,27 @@ class Extension(extensions.Extension):
             return {region.key: region_data(region) for region in regions}
 
         cls.add_to_class('render_json', render_json)
+
+        @classmethod
+        def check(cls, **kwargs):
+            errors = super(self.model, cls).check(**kwargs)
+            errors.extend(cls._check_json_method())
+            return errors
+
+        @classmethod
+        def _check_json_method(cls, **kwargs):
+            """Check all registered content types have a `.json` method."""
+            message = (
+                'Feincms content has no `json` method, but the ' +
+                '`render_json` extension is active for model `{}`.'
+            ).format(cls)
+            for content_type in cls._feincms_content_types:
+                if not hasattr(content_type, 'json'):
+                    yield checks.Error(
+                        message,
+                        obj=content_type,
+                        id='feincms_extensions.E001',
+                    )
+
+        cls.add_to_class('check', check)
+        cls.add_to_class('_check_json_method', _check_json_method)
